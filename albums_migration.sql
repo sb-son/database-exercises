c6d6746ea7d2a4852ff21addb376df3824865ce4
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-23 18:23:38 -0600

    created a trigger if release date year is not between a min and a max

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,8 +4,20 @@ CREATE TABLE IF NOT EXISTS albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     record_name VARCHAR(150) NOT NULL,
-    release_date INT(4) CHECK (release_date BETWEEN 1800 and 9999) NOT NULL,
+    release_date INT(4) NOT NULL DEFAULT (YEAR(NOW())),
     sales FLOAT(10,2) NOT NULL,
     genre VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+DELIMITER $$
+CREATE TRIGGER albums_bi_trig
+    BEFORE INSERT ON albums
+    FOR EACH ROW
+BEGIN
+    IF NEW.release_date < 1800 OR NEW.release_date > YEAR(NOW()) THEN
+        SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'Invalid release date, it should be between 1800 and the current year';
+END IF;
+END$$
+DELIMITER ;
