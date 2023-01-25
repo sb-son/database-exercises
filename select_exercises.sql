281c90119d5c63bc9cb3d1792730accbf5343854
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-25 13:04:36 -0600

    added employees database and used select and where clauses to sort through them

--- select_exercises.sql
+++ select_exercises.sql
@@ -4,5 +4,5 @@ SELECT release_date AS 'Date Sgt. Pepper''s Lonely Hearts Club Band album was re
 SELECT genre AS 'Genre of album ''Thriller''' FROM albums WHERE record_name = 'Thriller';
 SELECT record_name AS '90''s albums' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 SELECT record_name AS 'Albums with less than 40 million sales' FROM albums WHERE sales < 40;
-SELECT record_name AS 'Albums with genre as ''Rock''' FROM albums WHERE genre = 'Rock'\G
+SELECT record_name AS 'Albums with genre as ''Rock''' FROM albums WHERE genre = 'Rock';
 SELECT * FROM albums;
