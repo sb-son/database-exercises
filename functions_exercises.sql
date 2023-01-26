b13c9f5fef8e8fb5d9e971a94f2906c841d1fb49
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-26 15:06:47 -0600

    completed group by exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,8 +2,8 @@ USE employees;
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no ASC;
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
-SELECT * FROM employees WHERE last_name LIKE CONCAT('E%', '%E') ORDER BY hire_date ASC;
-SELECT * FROM employees WHERE birth_date LIKE '____-12-25';
+SELECT CONCAT(first_name, ' ', last_name) AS 'Name' FROM employees WHERE last_name LIKE CONCAT('E%', '%E') ORDER BY hire_date ASC;
+SELECT * FROM employees WHERE  MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25;
 SELECT * FROM employees WHERE birth_date LIKE '____-12-25' AND hire_date LIKE '199%' ORDER BY hire_date DESC, birth_date ASC;
-SELECT last_name, first_name, DATEDIFF(NOW(), hire_date) FROM employees WHERE birth_date LIKE '____-12-25' AND hire_date LIKE '199%' ORDER BY hire_date DESC, birth_date ASC;
+SELECT last_name, first_name, DATEDIFF(NOW(), hire_date) FROM employees WHERE birth_date LIKE '____-12-25' AND hire_date LIKE '199%' ORDER BY hire_date DESC, birth_date;
 
---
+++ group_by_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+SELECT first_name, COUNT(*) FROM employees GROUP BY first_name ORDER BY COUNT(*) DESC;
+
+SELECT DISTINCT last_name FROM employees WHERE last_name LIKE CONCAT('E%', '%E') GROUP BY last_name, first_name;
+
+SELECT DISTINCT last_name, first_name FROM employees WHERE last_name LIKE CONCAT('E%', '%E') GROUP BY last_name, first_name;
+
+SELECT DISTINCT COUNT(last_name) AS '# of employees with last name', last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT COUNT(*) AS '# of people', gender FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') GROUP BY gender;
