90b58f25eba2f225a00c7f00b76d9a0f2b9e20fa
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-26 11:52:11 -0600

    completed functions exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,13 +1,9 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name ASC;
-SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya' ORDER BY first_name DESC;
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M' ORDER BY first_name, last_name ASC;
-
 SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no ASC;
 SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E' ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE last_name LIKE CONCAT('E%', '%E') ORDER BY hire_date ASC;
-
-SELECT * FROM employees WHERE last_name LIKE '%q%' ORDER BY last_name ASC;
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' ORDER BY last_name DESC;
+SELECT * FROM employees WHERE birth_date LIKE '____-12-25';
+SELECT * FROM employees WHERE birth_date LIKE '____-12-25' AND hire_date LIKE '199%' ORDER BY hire_date DESC, birth_date ASC;
+SELECT last_name, first_name, DATEDIFF(NOW(), hire_date) FROM employees WHERE birth_date LIKE '____-12-25' AND hire_date LIKE '199%' ORDER BY hire_date DESC, birth_date ASC;
 
