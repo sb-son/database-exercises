fd313db65f6feb086017ee174ffc48774ed8c63a
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-27 17:31:37 -0600

    completed aliases exercise

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,4 +1,4 @@
 USE employees;
 
-SELECT CONCAT(first_name, ' ', last_name) AS full_name, birth_date
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
 FROM employees LIMIT 10;
